8b9d933c0ba5deac4ca79899c5e9f520793317fd
JAprogramirane <ja.programirane@gmail.com>
2013-03-18 14:54:02 +0200

    Zad 1 18.03.2013g.
    Цикъл For

---
+++ Project1/Project1/Form1.vb
@@ -0,0 +1,18 @@
+﻿Public Class Form1
+
+    Private Sub Button1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button1.Click
+        Dim username As String
+        Dim pass As String
+        username = TextBox1.Text
+        pass = TextBox2.Text
+        If username = "fyrex" And pass = "1234" Then
+            Label1.Text = "Welcome"
+        End If
+    End Sub
+
+    Private Sub Button2_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button2.Click
+        For i = 1 To 100 Step 1
+            Label1.Text = Label1.Text & " " & i
+        Next
+    End Sub
+End Class
