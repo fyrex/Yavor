9f8e1b27bdb7c56697c8ecd24bd9a4b3c24f31bf
JAprogramirane <ja.programirane@gmail.com>
2013-03-18 15:53:27 +0200

    Zad 2 18.03

---
+++ Cycle for/Cycle for/Form1.vb
@@ -0,0 +1,28 @@
+﻿Public Class Form1
+
+    Private Sub Button1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button1.Click
+        For i = 1 To 20 Step 2
+            Label1.Text = Label1.Text & " " & i
+        Next
+    End Sub
+
+    Private Sub Button2_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button2.Click
+        For i = 1 To 30 Step 1
+            Label1.Text = "I will be a good student" & " " & Label1.Text
+        Next
+    End Sub
+
+    Private Sub Button3_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button3.Click
+        Dim sum As Integer
+        Dim a, b As Integer
+        a = TextBox1.Text
+        b = TextBox2.Text
+
+        For i = a To b Step 1
+            If i Mod 3 = 0 Or i Mod 7 = 0 Then
+                Label1.Text = Label1.Text & " " & i
+            End If
+
+        Next
+    End Sub
+End Class
---
+++ Cycle for/Cycle for/My Project/AssemblyInfo.vb
@@ -0,0 +1,35 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following 
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+
+<Assembly: AssemblyTitle("Cycle for")> 
+<Assembly: AssemblyDescription("")> 
+<Assembly: AssemblyCompany("Spge")> 
+<Assembly: AssemblyProduct("Cycle for")> 
+<Assembly: AssemblyCopyright("Copyright © Spge 2013")> 
+<Assembly: AssemblyTrademark("")> 
+
+<Assembly: ComVisible(False)>
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("db90b185-fc5d-49f0-827a-448ebb2bbc89")> 
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version 
+'      Build Number
+'      Revision
+'
+' You can specify all the values or you can default the Build and Revision Numbers 
+' by using the '*' as shown below:
+' <Assembly: AssemblyVersion("1.0.*")> 
+
+<Assembly: AssemblyVersion("1.0.0.0")> 
+<Assembly: AssemblyFileVersion("1.0.0.0")> 
